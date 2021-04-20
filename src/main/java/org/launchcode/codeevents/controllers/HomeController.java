c4a0da44d2c4f5319045daed8566c43b84ba4261
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-19 21:00:35 -0500

    fragments implemented for headers

--- src/main/java/org/launchcode/codeevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codeevents/controllers/HomeController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class HomeController {
-
+    public static String title = "Coding Events Homepage";
     @GetMapping
     public String index(Model model) {
+        model.addAttribute("title", title);
         return "index";
     }
 }
