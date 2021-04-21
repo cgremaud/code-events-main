35492d6012388463bda8cd11306455f992946723
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-20 19:41:38 -0500

    refactored EventController to use a model/class Event.

--- src/main/java/org/launchcode/codeevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codeevents/controllers/EventController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codeevents.controllers;
 
 
+import org.launchcode.codeevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +17,26 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<HashMap<String, String>> eventsList = new ArrayList<>(); //Should be a list of HashMaps though?
+//    private static List<HashMap<String, String>>eventsList = new ArrayList<>(); //Should be a list of HashMaps though?
+    private static List<Event> eventsList = new ArrayList<>();
+
+//    private static HashMap<String, String> event1 = new HashMap<>();
+
+//    public static void initializeEventsList() {
+////        event1.put("name", "WWDC");
+////        event1.put("description", "Apple's Wordwide Dev Conf");
+////        eventsList.add();
+//
+//    }
+
     @GetMapping
     public String index (Model model) {
 
-        HashMap<String, String> event1 = new HashMap<>();
-
-        event1.put("name", "WWDC");
-        event1.put("description", "Apples Word Wide Developer Conference");
-        eventsList.add(event1);
+//        HashMap<String, String> event1 = new HashMap<>();
+        //TODO this is causing an issue because it's being called and added every time a request is made to /events
+        //no clue how to get it to call ONCE and then leave eventsList alone. . .
+        //The solution will probably involve the M bit of MVC
+//        initializeEventsList();
 
 
         String title = "Events List: All";
@@ -35,17 +47,18 @@ public class EventController {
 
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
-
+        String title = "Create Event";
         return "events/create";
     }
 
     @PostMapping("create")
     public String generateNewEvent(@RequestParam String name, @RequestParam String description) {
-        HashMap<String, String> event = new HashMap<>();
-        event.put("name", name);
-        event.put("description", description);
+//        HashMap<String, String> event = new HashMap<>();
+//        event.put("name", name);
+//        event.put("description", description);
+        Event event = new Event(name, description);
         eventsList.add(event);
 
-        return "redirect:"; //might need to return redirect: here. Not sure
+        return "redirect:";
     }
 }
