703cf146f3638b3734b6b3c7f543bf5e098833f9
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-07 07:44:56 -0500

    moved fields into EventCategory. Need to update everything else and establish relationship.

--- src/main/java/org/launchcode/codeevents/models/Event.java
+++ src/main/java/org/launchcode/codeevents/models/Event.java
@@ -7,37 +7,18 @@ import javax.validation.constraints.*;
 @Entity
 public class Event extends AbstractEntity {
 
-    @NotBlank(message = "email cannot be blank")
-    @Email(message = "Invalid email address.") //this only checks to see if it's X@Y, doesn't need a .com or anything to pass validation.
-    private String contactEmail;
 
     @NotBlank(message = "Must have a name")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-
-    @Size(max = 500, message = "Description must be fewer than 500 characters")
-    private String description;
-
-    @NotBlank(message = "Please specify a location.")
-    private String location;
-
-    private boolean registrationRequired;
-
-    @Min(value = 1, message = "Number of attendees must be greater than 0")
-    private int attendees;
 
     @ManyToOne
     @NotNull(message = "Please select a category.")
     private EventCategory eventCategory;
 
 
-    public Event(String contactEmail, String name, String description, String location, boolean registrationRequired, int attendees, EventCategory eventCategory) {
-        this.contactEmail = contactEmail;
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.location = location;
-        this.registrationRequired = registrationRequired;
-        this.attendees = attendees;
         this.eventCategory = eventCategory;
     }
 
@@ -51,45 +32,7 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public int getAttendees() {
-        return attendees;
-    }
-
-    public void setAttendees(int attendees) {
-        this.attendees = attendees;
-    }
-
-    public boolean isRegistrationRequired() {
-        return registrationRequired;
-    }
-
-    public void setRegistrationRequired(boolean registrationRequired) {
-        this.registrationRequired = registrationRequired;
-    }
 
     public EventCategory getEventCategory() {
         return eventCategory;
@@ -102,7 +45,7 @@ public class Event extends AbstractEntity {
 
     @Override
     public String toString() {
-        return "name='" + name + '\'' +", description='" + description + '\'';
+        return "name='" + name + '\'' +", description='" + '\'';
     }
 
 
---
+++ src/main/java/org/launchcode/codeevents/models/EventDetails.java
@@ -0,0 +1,75 @@
+package org.launchcode.codeevents.models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class EventDetails extends AbstractEntity {
+    @NotBlank(message = "email cannot be blank")
+    @Email(message = "Invalid email address.") //this only checks to see if it's X@Y, doesn't need a .com or anything to pass validation.
+    private String contactEmail;
+
+    @Size(max = 500, message = "Description must be fewer than 500 characters")
+    private String description;
+
+    @NotBlank(message = "Please specify a location.")
+    private String location;
+
+    private boolean registrationRequired;
+
+    @Min(value = 1, message = "Number of attendees must be greater than 0")
+    private int attendees;
+
+    public EventDetails(String contactEmail, String description, String location, boolean registrationRequired, int attendees) {
+        this.contactEmail = contactEmail;
+        this.description = description;
+        this.location = location;
+        this.registrationRequired = registrationRequired;
+        this.attendees = attendees;
+    }
+
+    public EventDetails() {}
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
+    }
+}
